652033b1005eb4cadc7e4041d91e14dffd45d820
Döll, Sebastian <sebastian@katallaxie.me>
2018-09-13 10:26:28 +0200

    chore ~ exp@30.0.0

--- src/app/config.js
+++ src/app/config.js
@@ -18,8 +18,34 @@ export const pkg = {
       'react-test-renderer': '^16.4.2'
     },
     dependencies: {
-      'react-native': 'https://github.com/expo/react-native/archive/sdk-29.0.0.tar.gz',
-      'react': '^16.3.1',
+      'react-native':
+        'https://github.com/expo/react-native/archive/sdk-29.0.0.tar.gz',
+      react: '^16.3.1',
+      '@expo/vector-icons': '^6.3.1'
+    }
+  },
+  '30.0.0': {
+    devDependencies: {
+      'babel-eslint': '^9.0.0',
+      'babel-plugin-module-alias': '^1.6.0',
+      eslint: '^5.5.0',
+      expo: '^30.0.0',
+      'eslint-config-prettier': '^3.0.1',
+      'eslint-plugin-import': '^2.14.0',
+      'eslint-plugin-prettier': '^2.6.2',
+      'eslint-plugin-react': '^7.11.1',
+      'jest-expo': '^30.0.0',
+      prettier: '^1.14.2',
+      'prettier-eslint': '^8.8.2',
+      jest: '^23.5.0',
+      'jest-cli': '^23.5.0',
+      'react-native-scripts': '^1.14.0',
+      'react-test-renderer': '^16.4.2'
+    },
+    dependencies: {
+      'react-native':
+        'https://github.com/expo/react-native/archive/sdk-30.0.0.tar.gz',
+      react: '^16.3.1',
       '@expo/vector-icons': '^6.3.1'
     }
   }
